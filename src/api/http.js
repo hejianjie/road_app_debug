6ef126e43d58bee411daa2a8318b67bc6209703a
xlikc0328 <15104076298@163.com>
2020-12-13 23:55:17 +0800

    用户登录密码错误提示

--- src/api/http.js
+++ src/api/http.js
@@ -2,28 +2,28 @@ import axios from 'axios'
 
 // axios 配置
 axios.defaults.timeout = 500 * 1000
-axios.defaults.baseURL="http://47.99.65.198:8020"
+axios.defaults.baseURL = "http://47.99.65.198:8020"
 var qs = require('qs')
 
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
     // Do something before request is sent
     return config;
-  }, function (error) {
+}, function (error) {
     // Do something with request error
     return Promise.reject(error);
-  });
+});
 
 // Add a response interceptor
 axios.interceptors.response.use(function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     return response;
-  }, function (error) {
+}, function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     return Promise.reject(error);
-  });
+});
 
 // 处理状态码
 const handleStatus = ({ status, data = {} }) => {
