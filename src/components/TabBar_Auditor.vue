64f3aa43e37a38491e2696eca9f0196056fa95d0
xlikc0328 <15104076298@163.com>
2020-12-20 10:40:18 +0800

    核量界面图标更改

--- src/components/TabBar_Auditor.vue
+++ src/components/TabBar_Auditor.vue
@@ -1,48 +1,55 @@
 <template>
-<div class="tab-bottm">
-  <ion-tabs >
+  <div class="tab-bottm">
+    <ion-tabs>
+      <!-- Match by "app.patrol" route name -->
+      <ion-tab tab="auditing"> </ion-tab>
 
-    <!-- Match by "app.patrol" route name -->
-    <ion-tab tab="auditing" >
-      
-    </ion-tab>
+      <!-- Match by an array of route names -->
+      <ion-tab tab="settings" :routes="['app.settings']">
+        <Settings />
+      </ion-tab>
 
-    <!-- Match by an array of route names -->
-    <ion-tab tab="settings" :routes="['app.settings']">
-      <Settings />
-    </ion-tab>
- 
-    <!-- Use v-slot:bottom with Vue ^2.6.0 -->
-    <template v-slot:bottom>
-      <ion-tab-bar>
-        <!-- Provide a custom route to navigate to -->
-        <ion-tab-button tab="auditing">
-          <ion-icon name="calendar"></ion-icon>
-          <ion-label>核量</ion-label>
-        </ion-tab-button>
+      <!-- Use v-slot:bottom with Vue ^2.6.0 -->
+      <template v-slot:bottom>
+        <ion-tab-bar>
+          <!-- Provide a custom route to navigate to -->
+          <ion-tab-button tab="auditing">
+            <!-- <ion-icon name="calendar"></ion-icon> -->
+            <img
+              src="https://static.easyicon.net/preview/129/1296761.gif"
+              alt=""
+              style="width: 20px; height: 20px"
+            />
+            <ion-label>核量</ion-label>
+          </ion-tab-button>
 
-        <!-- Provide extra data to route -->
-        <ion-tab-button tab="settings">
-          <ion-icon name="settings"></ion-icon>
-          <ion-label>我的</ion-label>
-        </ion-tab-button>
-      </ion-tab-bar>
-    </template>
-  </ion-tabs>
-</div>
+          <!-- Provide extra data to route -->
+          <ion-tab-button tab="settings">
+            <!-- <ion-icon name="settings"></ion-icon> -->
+            <img
+              src="https://static.easyicon.net/preview/113/1133857.gif"
+              alt=""
+              style="width: 20px; height: 20px"
+            />
+            <ion-label>我的</ion-label>
+          </ion-tab-button>
+        </ion-tab-bar>
+      </template>
+    </ion-tabs>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'TabBar'
-}
+  name: "TabBar",
+};
 </script>
 <style scoped>
-  .tab-bottm {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    height: 50px;
-  }
+.tab-bottm {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+}
 </style>
